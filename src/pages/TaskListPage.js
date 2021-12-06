1c350219015bdb817df3228e33ed5c24a6342708
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2021-12-06 14:24:58 -0300

    Cria listagem de tarefas

--- src/pages/TaskListPage.js
+++ src/pages/TaskListPage.js
@@ -1,9 +1,104 @@
+import { useEffect, useState } from 'react';
+import { Layout, Row, Col, Table, Modal, Button } from 'antd';
+import axios from 'axios';
+
+const { Content } = Layout;
+const { Column } = Table;
+
 const TaskListPage = () => {
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const requestTasks = async () => {
+    try {
+      setLoading(true);
+
+      const response = await axios.get('/tarefas');
+      setTasks(response.data);
+
+    } catch (error) {
+      console.warn(error);
+      Modal.error({
+        title: 'Não foi possível carregar suas tarefas, tente novamente mais tarde.'
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    requestTasks();
+  }, []);
+
+  const completeTask = async (taskId) => {
+    try {
+      setLoading(true);
+
+      await axios.put('/tarefas/' + taskId + '/conclusao');
+
+      await requestTasks();
+    } catch (error) {
+      console.warn(error);
+      Modal.error({
+        title: 'Não foi possível concluir a tarefa, tente novamente mais tarde.'
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const renderCompleteTask = (concluida, task) => {
     return (
-        <div>
-            Aqui vai a tabela com as tarefas cadastradas
-        </div>
+      <Button
+        onClick={() => {
+          completeTask(task.id);
+        }}
+      >
+        {concluida ? '✅' : '❌'}
+      </Button>
     );
+  };
+
+  return (
+    <Content>
+      <Row gutter={[24, 24]} justify="center">
+        <Col span={23}>
+
+          <Table
+            dataSource={tasks}
+            pagination={false}
+            loading={loading}
+          >
+            <Column
+              title="ID"
+              dataIndex="id"
+              key="id"
+            />
+            <Column
+              title="Titulo"
+              dataIndex="titulo"
+              key="titulo"
+            />
+            <Column
+              title="Criada em"
+              dataIndex="data_criacao"
+              key="data_criacao"
+              render={dataCriacao => {
+                return new Date(dataCriacao).toLocaleString();
+              }}
+            />
+            <Column
+              title="Concluída"
+              dataIndex="concluida"
+              key="concluida"
+              render={renderCompleteTask}
+            />
+          </Table>
+
+        </Col>
+      </Row>
+    </Content>
+  );
 }
 
 export default TaskListPage;
